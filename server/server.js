8972f3e73e1ccda3523b3c8ba45b187c85bc31cc
shiraz <shirazyousuf1234@gmail.com>
2023-09-04 22:56:12 +0530

    first version 1.5

--- server/server.js
+++ server/server.js
@@ -20,10 +20,10 @@ app.use((req, res, next) => {
 
 app.use('/api', taskRouter);
 
-app.use(express.static(path.join(__dirname,"../dist")));
+app.use(express.static(path.join(__dirname,"./dist")));
 
 app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "../dist/index.html"))
+    res.sendFile(path.join(__dirname, "./dist/index.html"))
 });
 
 app.listen(port, () => {
